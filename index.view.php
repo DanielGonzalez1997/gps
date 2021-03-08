c566da47a9488d5367b9ed0565a41a0d82a758da
DanielGonzalez1997 <80289709+DanielGonzalez1997@users.noreply.github.com>
2021-03-08 15:51:16 -0600

    Update index.view.php

--- index.view.php
+++ index.view.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<!--<meta name="viewport" content="width=device=width, initial-scale=1, shrink-tofit=no">-->
 	<meta name="description" content="">
-	<title>Proyecto</title>
+	<title>Proyecto tonto</title>
 	<script type="text/javascript"src="jquery-3.3.1.js"></script>
 	<script type="text/javascript"src="jquery-3.5.1.js"></script>
 	<script type="text/javascript"src="jquery.dataTables.min.js"></script>
